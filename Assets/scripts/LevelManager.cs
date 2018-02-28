773748b9848168f736cade18bbd1488ffe470643
Unknown <fireblaze123@gmail.com>
2018-02-28 14:53:44 +0100

    Level 3 and font

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -9,6 +9,10 @@ public class LevelManager : MonoBehaviour {
     public Light aura;
     public Object shadow;
     public AudioClip growl;
+    public float level1Time;
+    public float level2Time;
+    public float level3Time;
+    public float level4Time;
 
     private AudioSource audiosource;
     private ObjectSpawn spawner;
@@ -20,7 +24,7 @@ public class LevelManager : MonoBehaviour {
     void Start ()
     {
         tutorial = true;
-        audiosource = GameObject.FindGameObjectWithTag("SFX Manager").GetComponent<AudioSource>();
+        audiosource = GameObject.FindGameObjectWithTag("SFX Manager").GetComponents<AudioSource>()[1];
         startOfLevel = Time.time;
         level = 1;
         spawner = GameObject.FindGameObjectWithTag("Spawn").GetComponent<ObjectSpawn>();
@@ -55,7 +59,7 @@ public class LevelManager : MonoBehaviour {
                 {
                     aura.cookieSize -= 0.01f;
                 }
-                if (Time.time - startOfLevel >= 60)
+                if (Time.time - startOfLevel >= level1Time)
                 {
                     StartCoroutine(FirstScene());
                     level = 0;
@@ -68,11 +72,11 @@ public class LevelManager : MonoBehaviour {
             if (Time.time >= timestamp)
             {
                 int randNum = Random.Range(0, 100);
-                if (randNum < 70)
+                if (randNum < 50)
                 {
                     spawner.SpawnSharkRandom(15);
                 }
-                else if (randNum >= 70 && randNum < 80)
+                else if (randNum >= 50 && randNum < 80)
                 {
                     spawner.SpawnSquidRandom();
                 }
@@ -92,7 +96,7 @@ public class LevelManager : MonoBehaviour {
             {
                 aura.cookieSize -= 0.01f;
             }
-            if (Time.time - startOfLevel >= 60)
+            if (Time.time - startOfLevel >= level2Time)
             {
                 StartCoroutine(SecondScene());
                 level = 0;
@@ -100,7 +104,38 @@ public class LevelManager : MonoBehaviour {
         }
         else if (level == 3)
         {
+            if (Time.time >= timestamp)
+            {
+                int randNum = Random.Range(0, 100);
+                if (randNum < 40)
+                {
+                    spawner.SpawnSharkRandom(15);
+                }
+                else if (randNum >= 40 && randNum < 80)
+                {
+                    spawner.SpawnSquidRandom();
+                }
+                else if (randNum >= 80 && randNum < 90)
+                {
+                    spawner.SpawnCrateRandom();
+                }
+                else
+                {
+                    spawner.SpawnCliffsRandom(Random.Range(1, 3));
+                }
+                timestamp = Time.time + cooldown;
+                cooldown = Random.Range(0.5f, 2.0f);
+            }
 
+            if (aura.cookieSize > 8)
+            {
+                aura.cookieSize -= 0.01f;
+            }
+            if (Time.time - startOfLevel >= level3Time)
+            {
+                StartCoroutine(ThirdScene());
+                level = 0;
+            }
         }
         else if (level == 4)
         {
@@ -111,7 +146,7 @@ public class LevelManager : MonoBehaviour {
     IEnumerator Tutorial()
     {
         yield return new WaitForSeconds(3);
-        instructions.text = "Move the boat with WASD";
+        instructions.text = "Move the boat with the WASD keys";
         yield return new WaitForSeconds(5);
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
@@ -124,13 +159,13 @@ public class LevelManager : MonoBehaviour {
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
         instructions.text = "Shine your light on hidden objects to reveal their true nature";
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         spawner.SpawnSharkTutorial(65, 15);
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(5);
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
         instructions.text = "Keeping the spotlight turned on drains your battery";
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(5);
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
         instructions.text = "Picking up crates grants you single-use abilities";
@@ -139,11 +174,11 @@ public class LevelManager : MonoBehaviour {
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
         instructions.text = "The ability menu shows which abilities can be used \n Press the corresponding keys to select and ability";
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(6);
         instructions.text = "";
         yield return new WaitForSeconds(0.5f);
         instructions.text = "The different abilities are, respectively, battery recharge, repair kit, increased spotlight spread, and a siren that scares away enemies";
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(8f);
         instructions.text = "";
         yield return new WaitForSeconds(5f);
         instructions.text = "Level 1";
@@ -181,5 +216,10 @@ public class LevelManager : MonoBehaviour {
         instructions.text = "";
         level = 3;
         startOfLevel = Time.time;
+    }
+
+    IEnumerator ThirdScene()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
